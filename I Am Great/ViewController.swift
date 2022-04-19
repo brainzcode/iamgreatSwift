49315b80aa0dbe4e6e6dbd7a2880a3b8e6507641
brainzcode <98150590+brainzcode@users.noreply.github.com>
2022-04-19 23:49:04 +0100

    Initial Commit

---
+++ I Am Great/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Great
+//
+//  Created by SOG on 4/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
